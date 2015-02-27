1b44dfa49888a0c245b7c05146896aa6e5f15882
Carl Fairclough <contact@carlfairclough.me>
2015-02-27 10:47:38 +0000

    Contact style change

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+$(document).ready(function() {
+	$('.contact-button').click(function(e){
+		e.preventDefault();
 
+		$('footer').find('.contactme').addClass('active-form');
+		t = $(this)
+		tH = t.height();
+		$(this).animate({'opacity' : 0}, 250);
+		window.setTimeout( function(){
+			t.remove();
+			$('form').removeClass('hidden');
+		}, 700);
+	})
+})
