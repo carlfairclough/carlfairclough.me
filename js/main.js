e8d8c347b3554c3430fe80ade0c59ee0dad8f85c
Carl Fairclough <contact@carlfairclough.me>
2015-02-27 11:59:54 +0000

    Almost ready to deploy

--- js/main.js
+++ js/main.js
@@ -3,12 +3,28 @@ $(document).ready(function() {
 		e.preventDefault();
 
 		$('footer').find('.contactme').addClass('active-form');
-		t = $(this)
-		tH = t.height();
+		var t = $(this);
+		var tH = t.height();
 		$(this).animate({'opacity' : 0}, 250);
 		window.setTimeout( function(){
-			t.remove();
+			t.css({'display' : 'none'});
 			$('form').removeClass('hidden');
+			$('.cancel-form').css({'opacity' : 1})
+		}, 700);
+	})
+
+	$('.cancel-form').click(function(e){
+		e.preventDefault();
+
+		$('footer').find('.contactme').removeClass('active-form');
+		var t = $(this);
+		var tH = t.height();
+		$(this).animate({'opacity' : 0}, 250);
+		window.setTimeout( function(){
+			$('.contact-button').css({'opacity' : 0});
+		$('.contact-button').css({'display' : 'inline-block'});
+			$('.contact-button').animate({'opacity' : 1}, 250);
+			$('form').addClass('hidden');
 		}, 700);
 	})
 })
