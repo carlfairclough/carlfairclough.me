23dab41253de309fc9a545a8a64a8825334030b6
Carl Fairclough <contact@carlfairclough.me>
2015-03-01 06:28:08 +0000

    rejigging images and editing page descriptions

--- template/header.php
+++ template/header.php
@@ -6,8 +6,25 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Carl Fairclough | Designer</title>
-        <meta name="description" content="Carl Fairclough is a digital designer from Manchester, focusing on app design and everything that it entails.">
+        
+        <?php if (project == 'formisimo') { ?>
+        <title>Carl Fairclough | Formisimo Design</title>
+        <meta name="description" content="Overview of Carl Fairclough's role in the design of Formisimo, over the course of 6 months.">
+        <?php } elseif (project == 'textburst') { ?>
+        <title>Carl Fairclough | Textburst Design</title>
+        <meta name="description" content="Overview of Carl Fairclough's role in the design of Textburst, over the course of 18 months.">
+        <?php } elseif (project == 'champio') { ?>
+        <title>Carl Fairclough | Champio Design</title>
+        <meta name="description" content="Overview of Carl Fairclough's role in the design of Champio, over the course of 4 weeks.">
+        <?php } elseif (project == 'timetastic') { ?>
+        <title>Carl Fairclough | Timetastic Design</title>
+        <meta name="description" content="Overview of Carl Fairclough's role in the design of Timetastic, over the course of 18 months.">
+        <?php } else { ?>
+            <title>Carl Fairclough | Designer</title>
+            <meta name="description" content="Carl Fairclough is a digital designer from Manchester, focusing on app design and everything that it entails.">
+        <?php }; ?>
+        
+
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
