aa1cf29bda788dca75df23d303f6e46dc882ac46
Carl Fairclough <contact@carlfairclough.me>
2015-05-27 15:17:27 -0400

    GA code change and deleting old code

--- template/footer.php
+++ template/footer.php
@@ -5,73 +5,6 @@
                     <h1>Let's work together</h1>
                     <p>If you have a project that you'd like me to work on, then get in touch. I'm highly flexible and can accomodate for a wide variety of needs.
                         <br><a class="email-me" href="mailto:hi@carlfairclough.me" style="opacity:1; color:#222; cursor:pointer; margin-top:15px;">hi@carlfairclough.me</a></p>
-                    <!-- <a href="#contact" class="button contact-button">Use the form</a> -->
-
-                    <form method="POST" action="http://formspree.io/hi@carlfairclough.me" class="hidden">
-
-                        <div class="row">
-                            <div class="col-6 sma-col-12">
-                                <div class="fieldwrap">
-                                    <label>Full Name</label>
-                                    <input type="text" name="name">
-                                </div>
-                            </div>
-
-                            <div class="col-6 sma-col-12">
-                                <div class="fieldwrap">
-                                    <label>Email Address</label>
-                                    <input type="email" name="_replyto">
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            <div class="col-6 sma-col-12">
-                                <label class="radio-label">Budget:</label>
-                                <input type="radio" name="value" id="1000" value="£1000 - £2000">
-                                <label class="radio" for="1000"><span class="fakeBox"></span><span class="text-label pl10">£1000 - £2000</span></label>
-
-                                <input type="radio" name="value" id="2000" value="£2000 - £3000">
-                                <label class="radio" for="2000"><span class="fakeBox"></span><span class="text-label pl10">£2000 - £3000</span></label>
-
-                                <input type="radio" name="value" id="3000" value="£3000 - £5000">
-                                <label class="radio" for="3000"><span class="fakeBox"></span><span class="text-label pl10">£3000 - £5000</span></label>
-
-                                <input type="radio" name="value" id="5000" value="£5000+">
-                                <label class="radio" for="5000"><span class="fakeBox"></span><span class="text-label pl10">£5000+</span></label>
-
-                                <input type="radio" name="value" id="unsure" value="£1000 - £2000">
-                                <label class="radio" for="unsure"><span class="fakeBox"></span><span class="text-label pl10">Usure</span></label>
-                            </div>
-
-                            <div class="col-6 sma-col-12">
-                                <label class="radio-label">Timescale:</label>
-                                <input type="radio" name="timescale" id="2weeks" value="2 weeks">
-                                <label class="radio" for="2weeks"><span class="fakeBox"></span><span class="text-label pl10">2 weeks</span></label>
-
-                                <input type="radio" name="timescale" id="1month" value="1 month">
-                                <label class="radio" for="1month"><span class="fakeBox"></span><span class="text-label pl10">1 month</span></label>
-
-                                <input type="radio" name="timescale" id="2months" value="2 months">
-                                <label class="radio" for="2months"><span class="fakeBox"></span><span class="text-label pl10">2 months</span></label>
-
-                                <input type="radio" name="timescale" id="2monthsplus" value="2 months +">
-                                <label class="radio" for="2monthsplus"><span class="fakeBox"></span><span class="text-label pl10">2 months +</span></label>
-
-                                <input type="radio" name="timescale" id="flexible" value="Flexible">
-                                <label class="radio" for="flexible"><span class="fakeBox"></span><span class="text-label pl10">Flexible</span></label>
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            <div class="col-12">
-                                <label class="textarea-label">Enquiry</label>
-                                <textarea type="text" name="enquiry"></textarea>
-                                <input type="submit" value="Send" class="button">
-                                <input type="button" value="Cancel" class="button secondary cancel-form">
-                            </div>
-                        </div> 
-                    </form> 
                 </div>
             </div>
         </section>
@@ -81,18 +14,17 @@
         </footer>
 
 
-        <script type="text/javascript">
-              var _gaq = _gaq || [];
-              _gaq.push(['_setAccount', 'UA-34597368-1']);
-              _gaq.push(['_setDomainName', 'carlfairclough.me']);
-              _gaq.push(['_trackPageview']);
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-              (function() {
-                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-              })();
-        </script>
+	  ga('create', 'UA-34597368-1', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
+
         <script type="text/javascript">
             var sc_project=8690342; 
             var sc_invisible=1; 
