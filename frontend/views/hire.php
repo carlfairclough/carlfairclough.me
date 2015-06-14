2d8902007e80ba3b832861c684e96d943900f0ae
Carl Fairclough <contact@carlfairclough.me>
2015-06-14 12:55:42 -0400

    Drop pricing from hire page

--- frontend/views/hire.php
+++ frontend/views/hire.php
@@ -12,16 +12,15 @@
                     <h2>Design & Front-End Development projects</h2>
                     <p>These are my forté. Whether you need an extra person to see a project through or somebody to work on a project that you don’t have the expertise for, I'd love to help.</p>
                     <p>I prefer to work remotely, but I’m happy to travel to most places on a short-term basis to work with teams &amp; meet face-to-face.</p>
-                    <p>Rate:<span class="light-highlight"> £230 per day. </span></p>
                     <a class="button" href="mailto:hi@carlfairclough.me?subject=Freelance request">Book now</a>
                     
-                    
+<!--                    
                     <h2>Consulting</h2>
                     <p>As a result of working with a variety of different teams, I can identify Design &amp; Usability problems then make improvement suggestions which would improve your offering and help acheive your goals. I’m also aware of where front-end development headaches occur and can work with you to ensure that these don’t happen.</p>
                     <p>After the consultation, I will supply you with a document covering issues &amp; recommendations, as well as methods to best implement these. I can consult remotely.</p>
                     <p>Rate:<span class="light-highlight"> £145 per hour </span>(minimum of 2 hours)</p>
                     <a class="button" href="mailto:hi@carlfairclough.me?subject=Consulting request">Book now</a>
-                
+-->                
                 </div>
             </div>
         </section>
